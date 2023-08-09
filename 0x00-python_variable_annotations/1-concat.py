98d63a7d3256093ea74f8c51980e8f3bcb000210
Augustine Hebrus <augustineebuka98@gmail.com>
2023-08-09 16:04:59 +0000

    added task 2

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,8 +2,8 @@
 '''
 task2: concate string
 '''
-def concat(str1: str, str2:str)-> str:
+def concat(str1: str, str2: str)-> str:
     '''
      func that concate str
     '''
-    return str1+str2
+    return str1 + str2
