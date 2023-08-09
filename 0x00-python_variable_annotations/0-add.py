61b23db42f84de3fa8440ee16577449c25688d31
Obetta AugusTine Ebuka <78606987+Augustine-ebuka@users.noreply.github.com>
2023-08-09 17:15:58 +0100

    added complete files

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""task 1: add a and b using mypy
+'''Task 0's module.
+'''
 
-"""
+
 def add(a: float, b: float) -> float:
     '''Adds two floating-point numbers.
     '''
     return a + b
-
